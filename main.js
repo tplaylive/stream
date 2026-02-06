656ba454da43772788b3fd30d9e6bf4dd4f1c67d
T Play <210236050+tplaylive@users.noreply.github.com>
2026-02-07 01:59:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const channels = [
+            { 
+                name: "CNN",
+                sources: [
+                    { 
+                        name: "USA", 
+                        url: "https://qp-pldt-live-grp-12-prod.akamaized.net/out/u/dr_cnnhd.mpd",
+                        type: "dash",
+                        drm: {
+                            kid: "900c43f0e02742dd854148b7a75abbec",
+                            key: "da315cca7f2902b4de23199718ed7e90"
+                        }
+                    }
+                ],
+                img: "https://play-lh.googleusercontent.com/375NW5yL8owK_hW9igW9sh-YJbda9ZcygpDXuVvK_R7l-yJp-fuhb4qvUw_FE4XW4ms", 
+                category: "News",
+                description: "World News"
+            },
                {
                 name: "BBC News", 
                 sources: [
-                    { name: "Server 1", url: "https://tplay.live/out/news/bbc-news/index.m3u8" },
-                    { name: "Server 2", url: "https://cdn4.skygo.mn/live/disk1/BBC_News/HLSv3-FTA/BBC_News.m3u8" }
+                    { name: "UK", url: "https://tplay.live/out/news/bbc-news/index.m3u8" },
+                    { name: "Asia", url: "https://cdn4.skygo.mn/live/disk1/BBC_News/HLSv3-FTA/BBC_News.m3u8" }
                 ], 
                 img: "https://i.postimg.cc/s2679ZP4/bbc.jpg", 
                 category: "News",
                 package: ["150","250","500"],
-                description: "UK"
+                description: "World News"
              },
              { 
                 name: "DD News", 
