a02bf42343c68882d4ec5d30c1598418c31e83df
T Play <210236050+tplaylive@users.noreply.github.com>
2026-02-07 00:11:27 +0800

    Create codes.js

---
+++ codes.js
@@ -0,0 +1,16 @@
+// ===== ACCESS CODES DATABASE =====
+// You control codes here
+
+const ACCESS_CODES = {
+  "STD-12345": {
+    tier: "250",
+    maxDevices: 5,
+    devices: []
+  },
+
+  "PREM-99999": {
+    tier: "500",
+    maxDevices: 5,
+    devices: []
+  }
+};
