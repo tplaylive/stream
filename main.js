a5e287cde19c1fa32044fa7c0209be05648fa27c
T Play <210236050+tplaylive@users.noreply.github.com>
2026-03-23 14:07:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4076,18 +4076,11 @@ const channels = [
             { 
                 name: "Willow", 
                 sources: [
-                    { 
-                        name: "Server 1", 
-                        url: "https://a201aivottlinear-a.akamaihd.net/OTTB/lhr-nitro/clients/dash/enc/f60kqesunw/out/v1/a435ed7a00f947deb4369b46d8f2fb70/cenc.mpd",
-                        type: "dash",
-                        drm: {
-                            kid: "1779c27b9d077a3ba0c9cc1bb9a94b9f",
-                            key: "cc5cf3b7928fb9e0a1ee6a8b566f0a8e"
-                        }
-                    }
+                    { name: "Server 1", url: "https://d36r8jifhgsk5j.cloudfront.net/Willow_TV.m3u8" }
                 ], 
                 img: "https://i.postimg.cc/L4b8S5Df/willow.jpg", 
                 category: "Sports",
+                package: ["150","250","500"],
                 description: "World"
             },
             { 
