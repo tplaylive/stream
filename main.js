577b23ed57f9c1e4694149c9eeb1fa37f703b80c
T Play <210236050+tplaylive@users.noreply.github.com>
2026-04-12 00:55:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -987,7 +987,7 @@ const channels = [
             {
                 name: "Sony Yay",
                 sources: [
-                    { name: "Server 1", url: "https://tvsky-bostaflix.global.ssl.fastly.net/sliv/stream.m3u8?id=1000001971&e=.m3u8" },
+                    { name: "Server 1", url: "https://mhdtvhub.com/sliv/stream.php?id=1000001971&e=.m3u8" },
                     { name: "Server 2", url: "https://autumn-shape-b04a.soft-disk-1347.workers.dev/?url=http://maxotts.maxdigitaltv.com/x-media/C39/master.m3u8" },
                     { name: "Server 3", url: "https://satoshi-cors.herokuapp.com/http://103.99.249.139/sonyyay/index.m3u8" },
                     { name: "Server 4", url: "https://anywhere.pwisetthon.com/http://103.99.249.139/sonyyay/index.m3u8" },
